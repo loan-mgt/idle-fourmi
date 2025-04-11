cac8fed192f661c4c00b6eac7022fa88827d58a5
Noé <noe.favier@outlook.com>
2025-04-11 19:44:51 +0200

    fix(canvas): fixed content

--- frontend/src/components/canva.component.vue
+++ frontend/src/components/canva.component.vue
@@ -1,43 +1,49 @@
-<script>
-export default {
-    name: "CanvaComponent",
-};
+<template>
+    <div ref="canvasContainer"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
 import { Application, Assets, Sprite } from 'pixi.js';
 
-(async () =>
-{
-  // Create a new application
-  const app = new Application();
+const canvasContainer = ref(null);
 
-  // Initialize the application
-  await app.init({ background: '#1099bb', resizeTo: window });
+onMounted(() => {
+    (async () => {
+        // Create a new application
+        const app = new Application();
 
-  // Append the application canvas to the document body
-  document.body.appendChild(app.canvas);
+        // Initialize the application
+        await app.init({ background: '#1099bb', resizeTo: window });
 
-  // Load the bunny texture
-  const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+        // Append the application canvas to the container
+        app.canvas.className = 'pixi-canvas';
+        app.canvas.style.width = '99%';
+        app.canvas.style.height = '99%';
+        canvasContainer.value.appendChild(app.canvas);
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+        // Load the bunny texture
+        const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
+        // Create a bunny Sprite
+        const bunny = new Sprite(texture);
 
-  // Move the sprite to the center of the screen
-  bunny.x = app.screen.width / 2;
-  bunny.y = app.screen.height / 2;
+        // Center the sprite's anchor point
+        bunny.anchor.set(0.5);
 
-  app.stage.addChild(bunny);
+        // Move the sprite to the center of the screen
+        bunny.x = app.screen.width / 2;
+        bunny.y = app.screen.height / 2;
 
-  // Listen for animate update
-  app.ticker.add((time) =>
-  {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
-})();
+        app.stage.addChild(bunny);
+
+        // Listen for animate update
+        app.ticker.add((time) => {
+            // Just for fun, let's rotate mr rabbit a little.
+            bunny.rotation += 0.1 * time.deltaTime;
+        });
+    })();
+});
 </script>
 
+<style scoped></style>
